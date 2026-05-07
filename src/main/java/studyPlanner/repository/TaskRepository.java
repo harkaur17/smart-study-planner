6de94dd9ce5ea5c94dcff19e753a6b11c045f98d
harkaur17 <kaurharleen.9809@gmail.com>
2026-05-07 15:48:26 -0400

    Task undo, recalculates the streak

--- src/main/java/studyPlanner/repository/TaskRepository.java
+++ src/main/java/studyPlanner/repository/TaskRepository.java
@@ -3,10 +3,15 @@ package studyPlanner.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import studyPlanner.model.Task;
 import studyPlanner.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByUser(User user);
 
     List<Task> findByUserOrderByDueDateAsc(User user);
+
+    @Query("SELECT COUNT(t) FROM Task t WHERE t.user = :user AND t.status = studyPlanner.model.Task$Status.DONE")
+    long countDoneTasksByUser(@Param("user") User user);
 }
